bd5282868b166573d50e957bf8ea0ebf76d9854f
chaggle <chaggle@foxmail.com>
2023-07-23 23:54:48 +0800

    [feat] oauth2 更新授权认证

--- oauth2/src/main/java/com/chaggle/oauth2/config/WebSecurityConfig.java
+++ oauth2/src/main/java/com/chaggle/oauth2/config/WebSecurityConfig.java
@@ -1,15 +1,70 @@
 package com.chaggle.oauth2.config;
 
 
+import com.chaggle.oauth2.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    private UserService userService;
 
     @Bean
-    public AuthenticationManager authenticationManager() {
+    public AuthenticationManager authenticationManager()  {
         return null;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return passwordEncoder().encode(rawPassword);
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return passwordEncoder().matches(rawPassword, encodedPassword);
+            }
+        });
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-*")
+                .antMatchers("/register-*");
+//        web security 进行链条式 filter 一般在gateway 里面写，
+//         gateway 才是正确的拦截位置！基于 netty 进行响应式编程！
+//         里面进行原生的 oauth2 集成
+//        web.addSecurityFilterChainBuilder(null);
+//        web.expressionHandler(null);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated()
+                .and().httpBasic()
+                //cors 为跨域请求！
+                .and().cors()
+                //csrf 不支持 post！ 用 oauth2 一定需要将 csrf 禁止掉
+                .and().csrf().disable();
+
     }
 }
